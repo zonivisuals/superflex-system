247c9db707f696c5d7c53c1f30263903be759035
zonivisuals <mohamed.hajji.med@gmail.com>
2025-02-17 23:32:03 +0100

    setup the home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+export default function Home() {
+  const [inputValue, setInputValue] = useState('');
+  const [username, setUsername] = useState('');
+
+  const handleConfirm = () => {
+    if(!inputValue) return
+    setUsername(inputValue);
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-10 min-h-full">
+      
+      <div className='flex flex-col items-center justify-center gap-6'>
+        <h1 id='italic' className="text-5xl normal italic">
+          welcome to the system
+        </h1>
+
+        <p className="text-2xl">
+          please type a username
+        </p>
+      </div>
+
+      <div className="flex items-center justify-between h-10 gap-2">
+          <input
+            type="text"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            placeholder="Sung jinwoo"
+            className="italic h-full px-2 pb-3 text-center text-white bg-[rgba(255,255,255,0.16)] focus:outline-none placeholder-[rgba(255,255,255,0.7)]"
+          />
+          <button 
+            onClick={handleConfirm}
+            className="bg-white flex items-start justify-center px-12 h-full text-[#570078] italic"
+          >
+            confirm
+          </button>
+      </div>
+      
+    </div>
+  );
+}
